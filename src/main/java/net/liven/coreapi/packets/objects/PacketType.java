a62f1f1a3f83974f32cdaa497c2ad96f403de2a8
aryan <tiwariaryan035@gmail.com>
2025-05-14 19:37:44 +0530

    - fixed issues

---
+++ src/main/java/net/liven/coreapi/packets/objects/PacketType.java
@@ -0,0 +1,7 @@
+package net.liven.coreapi.packets.objects;
+
+public enum PacketType {
+    DATA_REQUEST_PACKET,
+    ACTION_REQUEST_PACKET;
+
+}
--- src/main/java/net/liven/coreapi/utils/PersistenceUtils.java
+++ src/main/java/net/liven/coreapi/utils/PersistenceUtils.java
@@ -4,7 +4,25 @@ import java.io.*;
 import java.util.Base64;
 import java.util.List;
 
-public class Base64Utils {
+public class PersistenceUtils {
+
+    public static byte[] toBytes(List<String> stringList) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(stringList);
+        return bos.toByteArray();
+    }
+
+    public static List<String> fromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+        if (bytes == null) {
+            return null;
+        }
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return (List<String>) ois.readObject();
+    }
+
+
     public static String encodeList(List<String> list) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
